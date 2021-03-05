7e038dbd6951603f7b3442ba879b927d8a0920ba
Rob Heslin <Rob.Heslin@gmail.com>
2021-03-05 16:26:01 -0500

    Removed TODO's that have been done awhile ago, removed old, commented-out constants that were not being used

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,17 +17,14 @@ package frc.robot;
  */
 public final class Constants {
     //Swerve conversion factors
-    // public static final double VOLTAGE_TO_RAD_CONV_FACTOR = 2*Math.PI/3.3; //Not needed since using CANCoder
-    // public static final double RAD_TO_DEG_CONV_FACTOR = 180/Math.PI;//removed in favor of Math.toDegrees()
-    // public static final double DEG_TO_RAD_CONV_FACTOR = Math.PI/180;//removed in favor of Math.toRadians()
-    public static final double RAD_TO_ENC_CONV_FACTOR = 10.17985; //0.00248835;//TODO:find the radian to enc factor (Mech Team) :Double check this!
+    public static final double RAD_TO_ENC_CONV_FACTOR = 10.17985; //the radian to enc factor
     public static final double DRIVE_ENC_TO_METERS_FACTOR = 1.0;//TODO:find this ratio from mechanicla specs
     public static final double PI_OVER_TWO = Math.PI/2;
     public static final double THREE_PI_OVER_TWO = 3*PI_OVER_TWO;
     public static final double TWO_PI = 2*Math.PI;
 
     //Swerve dimension factors, distances from center of mass(units must be in meters!)
-    public static final double X_POSITIVE_DISTANCE_FROM_CENTER = .294;//TODO:find this distance(Mech Team)
+    public static final double X_POSITIVE_DISTANCE_FROM_CENTER = .294;//this is to robot center
     public static final double Y_POSITIVE_DISTANCE_FROM_CENTER = .294;
     public static final double X_NEGATIVE_DISTANCE_FROM_CENTER = -.294;//THIS NUMBER MUST BE NEGATIVE!!!!!
     public static final double Y_NEGATIVE_DISTANCE_FROM_CENTER = -.294;//THIS NUMBER MUST BE NEGATIVE!!!!!
