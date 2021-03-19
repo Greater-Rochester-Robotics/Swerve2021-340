99e220dbcfe2b7a7505ae2c4f83d771b0a96cec5
Nathan M <nlm899999@gmail.com>
2021-03-19 18:16:30 -0400

    [Nate] fixed the code I broke

--- src/main/java/frc/robot/commands/Shooter/LowerHardStop.java
+++ src/main/java/frc/robot/commands/Shooter/LowerHardStop.java
@@ -22,7 +22,6 @@ public class LowerHardStop extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    RobotContainer.shooter.lowerHardStop();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
--- src/main/java/frc/robot/commands/Shooter/RaiseHardStop.java
+++ src/main/java/frc/robot/commands/Shooter/RaiseHardStop.java
@@ -22,7 +22,6 @@ public class RaiseHardStop extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    RobotContainer.shooter.raiseHardStop();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
