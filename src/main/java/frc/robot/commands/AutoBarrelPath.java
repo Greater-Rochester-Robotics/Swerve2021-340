b7a194e4313c09bfb0bfb09e6d9223f433ea71db
rob-heslin <Rob.Heslin@gmail.com>
2021-03-22 00:01:02 -0400

    [Rob]Added command groups for running autonav with simple commands

---
+++ src/main/java/frc/robot/commands/AutoBarrelPath.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * Running the barrel path using DriveStraight and DriveArc commands
+ */
+public class AutoBarrelPath extends SequentialCommandGroup {
+  /** Creates a new AutoBarrelPath. */
+  public AutoBarrelPath() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
+  }
+}
