b9b81c7cca295ca5e68bad4f98ed9e25ff5760ea
rob-heslin <Rob.Heslin@gmail.com>
2021-10-27 17:48:50 -0400

    [Rob]Untested additions made to ShootSwitchShoot, paths added for square motion

--- src/main/java/frc/robot/commands/auto/AutoShootThenSwitchThenShoot.java
+++ src/main/java/frc/robot/commands/auto/AutoShootThenSwitchThenShoot.java
@@ -5,10 +5,12 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.GetSmol;
 import frc.robot.commands.Drive.autoFunc.DrivePathWeaverProfile;
 import frc.robot.commands.Drive.autoFunc.DriveStraightenAllModules;
 import frc.robot.commands.Drive.autoFunc.DriveTurnToAngle;
+import frc.robot.commands.Drive.autoFunc.DriveTurnToTarget;
 import frc.robot.commands.Shooter.PrepHoodShot;
 import frc.robot.commands.Shooter.ProgTBallWithHintOfLime;
 import frc.robot.commands.Shooter.ResetBallsShot;
@@ -24,20 +26,39 @@ public class AutoShootThenSwitchThenShoot extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new ResetBallsShot(),
-      new PrepHoodShot(),
-      deadline(
-        new ProgTBallWithHintOfLime(0.0,true).withTimeout(2.5),
-        new DriveStraightenAllModules()
+      parallel(
+        new PrepHoodShot(),//start the wheel, get the hood up
+        sequence(
+          // new DriveTurnToAngle(.25).withTimeout(.5),//if we need this the value will be small
+          new DriveTurnToTarget().withTimeout(.5)//rotate to the target
+        )
       ),
+      new ProgTBallWithHintOfLime(0.0,false).withTimeout(1.5),//shoot with the hood up, till we're out of balls
       new GetSmol(),
+      new DriveTurnToAngle(0).withTimeout(1.5),//turn back to the starting position
+      new DriveStraightenAllModules().withTimeout(.5),
+      new DrivePathWeaverProfile("SwitchGrabpt1"),
       // race(
       //   new Load(),
         sequence(
           new DriveTurnToAngle(-1.1783),
-          new DrivePathWeaverProfile("SwitchGrabpt1")
+          new DrivePathWeaverProfile("SwitchGrabpt2"),
+          new WaitCommand(.75),//wait a moment to get the balls that might be stray
+          new DrivePathWeaverProfile("SwitchGrabpt3"),
+          new WaitCommand(.75)
         )
       // )
-      
+      ,
+      new DrivePathWeaverProfile("SwitchGrabpt4"),
+      parallel(
+        new PrepHoodShot(),//start the wheel, get the hood up
+        sequence(
+          new DriveTurnToAngle(1.1783).withTimeout(1.5),//if we need this the value will be small
+          new DriveTurnToTarget().withTimeout(.5)//rotate to the target
+        )
+      ),
+      new ProgTBallWithHintOfLime(0.0,false).withTimeout(1.5),//shoot with the hood up, till we're out of balls
+      new GetSmol()
     );
   }
 }
